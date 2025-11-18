0495f16fe331246555e5eb827a5a6ca06191ba37
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-11-18 22:40:28 +0000

    Package

--- pkgs/terraform-config-inspect/package.nix
+++ pkgs/terraform-config-inspect/package.nix
@@ -1,1 +1,26 @@
-{ pkgs }: true
+{
+  pkgs,
+  buildGoModule,
+  lib,
+  fetchFromGitHub,
+}:
+buildGoModule {
+  pname = "terraform-config-inspect";
+  version = "2.20.1";
+
+  src = fetchFromGitHub {
+    owner = "hashicorp";
+    repo = "terraform-config-inspect";
+    rev = "225c06ed5fd99341e37f199aed9c91e829c14be1";
+    hash = "sha256-3zl2rGij/DBwFOKCLQIkadgKhHYOA+e7v5qrU2qbd+g=";
+  };
+
+  vendorHash = "sha256-iYrSk9JqxvhYSJuSv/nhZep41gRr644ZzGFWXMGQgyc=";
+
+  meta = {
+    description = "Helper library for shallow inspection of Terraform configurations";
+    homepage = "https://github.com/hashicorp/terraform-config-inspect";
+    license = lib.licenses.mpl20;
+    maintainers = with lib.maintainers; [ drakon64 ];
+  };
+}
